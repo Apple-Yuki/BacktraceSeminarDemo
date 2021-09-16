0b4421fa02a8684e9fb830c94e5d46d86f09eb95
Shunsuke Sakai <shunsuke.sakai@unity3d.com>
2021-09-16 11:23:11 +0900

    Adds: Sample.cs

---
+++ Assets/Sample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        throw new InvalidOperationException("Logfile cannot be read-only");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
